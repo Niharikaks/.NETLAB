d3f7420780bceaf24c45cb2a39d3b2684fd2fa8e
Niharikaks <ksniharika696@gmail.com>
2026-03-05 10:43:54 +0530

    Add Student class with properties

---
+++ Student/Student.cs
@@ -0,0 +1,14 @@
+
+namespace Routing.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public int Marks { get; set; }
+    }
+}
